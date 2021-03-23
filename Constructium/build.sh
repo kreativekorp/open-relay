f03fa47b9523f4e09af9cadebb9fcab1e5679004
Rebecca G. Bettencourt <beckiejo@gmail.com>
2021-03-22 21:43:26 -0700

    Add PUAA to Constructium

--- Constructium/build.sh
+++ Constructium/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Find FontForge
 if command -v fontforge >/dev/null 2>&1; then
 	FONTFORGE="fontforge"
 elif test -f /Applications/FontForge.app/Contents/Resources/opt/local/bin/fontforge; then
@@ -9,6 +10,31 @@ else
 	exit 1
 fi
 
+# Find Bits'n'Picas
+if test -f BitsNPicas.jar; then
+	BITSNPICAS="java -jar BitsNPicas.jar"
+elif test -f ../BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../BitsNPicas/BitsNPicas.jar"
+elif test -f ../Workspace/BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../Workspace/BitsNPicas/BitsNPicas.jar"
+elif test -f ../../BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../BitsNPicas/BitsNPicas.jar"
+elif test -f ../../Workspace/BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../Workspace/BitsNPicas/BitsNPicas.jar"
+elif test -f ../../../BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../../BitsNPicas/BitsNPicas.jar"
+elif test -f ../../../Workspace/BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../../Workspace/BitsNPicas/BitsNPicas.jar"
+elif test -f ../../../../BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../../../BitsNPicas/BitsNPicas.jar"
+elif test -f ../../../../Workspace/BitsNPicas/BitsNPicas.jar; then
+	BITSNPICAS="java -jar ../../../../Workspace/BitsNPicas/BitsNPicas.jar"
+else
+	echo "Could not find BitsNPicas."
+	exit 1
+fi
+
+# Find ttf2eot
 if command -v ttf2eot >/dev/null 2>&1; then
 	TTF2EOT="ttf2eot"
 else
@@ -23,6 +49,11 @@ rm -rf constructium
 # Generate ttf
 $FONTFORGE -lang=ff -c 'i = 1; while (i < $argc); Open($argv[i]); Generate($argv[i]:r + ".ttf", "", 0); i = i+1; endloop' \
 	Constructium.sfd
+
+# Inject PUAA table
+$BITSNPICAS injectpuaa \
+	-D Blocks.txt UnicodeData.txt \
+	-I Constructium.ttf
 
 # Convert to eot
 $TTF2EOT < Constructium.ttf > Constructium.eot
