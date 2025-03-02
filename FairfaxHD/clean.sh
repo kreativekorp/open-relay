5067a20b55d75ff77b8faf7a420f3dc48be994be
TG635-alt126xA <tg635.alt126@gmail.com>
2025-03-02 09:32:41 +0700

    Clean FairfaxLogosHD

--- FairfaxHD/clean.sh
+++ FairfaxHD/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -f *.sfd-* *Tmp* *_base.* FairfaxHD.ttf FairfaxHD.eot FairfaxHD.zip FairfaxPonaHD.* FairfaxPulaHD.* FairfaxHaxHD.* FairfaxSMHD.*
+rm -f *.sfd-* *Tmp* *_base.* FairfaxHD.ttf FairfaxHD.eot FairfaxHD.zip FairfaxPonaHD.* FairfaxPulaHD.* FairfaxLogosHD.* FairfaxHaxHD.* FairfaxSMHD.*
 rm -rf fairfaxhd Android
