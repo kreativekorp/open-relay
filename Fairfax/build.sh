b487c2ca61b8d2259377a5351932302f12483e11
RebeccaRGB <beckiejo@gmail.com>
2026-02-05 17:44:10 -0800

    Make Fairfax Build Again

--- Fairfax/build.sh
+++ Fairfax/build.sh
@@ -43,7 +43,7 @@ else
 fi
 
 STRFTIME="python ../openrelay-tools/tools/strftime.py"
-SITELENPONA="python ../openrelay-tools/tools/sitelenpona.py"
+SITELENPANA="python ../openrelay-tools/tools/sitelenpana.py"
 BLOCKS="python ../openrelay-tools/tools/blocks.py"
 UNIDATA="python ../openrelay-tools/tools/unicodedata.py"
 PUAABOOK="python ../openrelay-tools/tools/puaabook.py"
@@ -53,55 +53,69 @@ PYPUAA="python ../openrelay-tools/tools/pypuaa.py"
 rm -f *_base.* Fairfax*.ttf Fairfax*.eot Fairfax*.zip
 rm -rf fairfax
 
-# Generate ttf
-$BITSNPICAS convertbitmap \
-	-f ttf -t 0xF800 -d 0x10FF40-0x10FFC0 \
+# Generate patched kbitx
+$BITSNPICAS convertbitmap -f kbitx \
+	-t 0xF800 -d 0x10FF40-0x10FFC0 \
+	-spc 0x0021-0x007E -spc 0x00B0 \
+	-spc 0x3001-0x3002 -spc uni3001.vs2 -spc uni3002.vs2 \
+	-spc 0x300C-0x300F -spc 0x309B-0x309C -spc 0xFF9E-0xFF9F \
+	-spc 0xF1900-0xF198F -spc 0xF199C-0xF199D -spc 0xF19A0-0xF19FF \
+	-spc uF1910.rand01 -spc uF1910.rand02 -spc uF1910.rand03 -spc uF1910.rand04 \
+	-spc 0xF1C40-0xF1C7D -spc 0xF1C80-0xF1C9F \
 	-s '20XX[.]XX[.]XX' -r `$STRFTIME '%Y.%m.%d'` \
 	-s '20XXXXXX' -r `$STRFTIME '%Y%m%d'` \
 	-s '20XX' -r `$STRFTIME '%Y'` \
-	-o Fairfax_base.ttf Fairfax.kbitx \
-	-o FairfaxBold_base.ttf FairfaxBold.kbitx \
-	-o FairfaxItalic_base.ttf FairfaxItalic.kbitx \
-	-o FairfaxSerif_base.ttf FairfaxSerif.kbitx \
+	-o Fairfax_base.kbitx Fairfax.kbitx \
+	-o FairfaxBold_base.kbitx FairfaxBold.kbitx \
+	-o FairfaxItalic_base.kbitx FairfaxItalic.kbitx \
+	-o FairfaxSerif_base.kbitx FairfaxSerif.kbitx
+
+# Generate fea
+$SITELENPANA -f Fairfax_base.kbitx -i ../features/sitelenpona.txt -a ../features/spascii.fea -o ../features/spbase.fea
+$SITELENPANA -f Fairfax_base.kbitx -i ../features/titipula.txt -a ../features/tpascii.fea -o /dev/null
+
+# Generate ttf
+$BITSNPICAS convertbitmap -f ttf \
+	-o Fairfax_base.ttf Fairfax_base.kbitx \
+	-o FairfaxBold_base.ttf FairfaxBold_base.kbitx \
+	-o FairfaxItalic_base.ttf FairfaxItalic_base.kbitx \
+	-o FairfaxSerif_base.ttf FairfaxSerif_base.kbitx \
 	-s 'Fairfax( Serif)?' -r '$0 Pona' \
-	-o FairfaxPona_base.ttf Fairfax.kbitx \
+	-o FairfaxPona_base.ttf Fairfax_base.kbitx \
 	-s ' Pona' -r ' Pula' \
-	-o FairfaxPula_base.ttf Fairfax.kbitx \
+	-o FairfaxPula_base.ttf Fairfax_base.kbitx \
 	-s ' Pula' -r ' Hax' \
-	-o FairfaxHax_base.ttf Fairfax.kbitx \
-	-o FairfaxHaxBold_base.ttf FairfaxBold.kbitx \
-	-o FairfaxHaxItalic_base.ttf FairfaxItalic.kbitx \
-	-o FairfaxSerifHax_base.ttf FairfaxSerif.kbitx \
+	-o FairfaxHax_base.ttf Fairfax_base.kbitx \
+	-o FairfaxHaxBold_base.ttf FairfaxBold_base.kbitx \
+	-o FairfaxHaxItalic_base.ttf FairfaxItalic_base.kbitx \
+	-o FairfaxSerifHax_base.ttf FairfaxSerif_base.kbitx \
 	-s ' Hax' -r ' SM' -c \
-	-o FairfaxSM_base.ttf Fairfax.kbitx \
-	-o FairfaxSMBold_base.ttf FairfaxBold.kbitx \
-	-o FairfaxSMItalic_base.ttf FairfaxItalic.kbitx \
-	-o FairfaxSerifSM_base.ttf FairfaxSerif.kbitx
+	-o FairfaxSM_base.ttf Fairfax_base.kbitx \
+	-o FairfaxSMBold_base.ttf FairfaxBold_base.kbitx \
+	-o FairfaxSMItalic_base.ttf FairfaxItalic_base.kbitx \
+	-o FairfaxSerifSM_base.ttf FairfaxSerif_base.kbitx
 
-# Add OpenType features (Bits'n'Picas cannot do this itself)
-$SITELENPONA -a ../features/asuki.txt -t ../features/atuki.txt -e ../features/extendable.txt -j ../features/joiners.txt -g Fairfax.kbitx
-cat ../features/languages.fea ../features/sequences.fea joiners.fea                           ../features/slc-variants.fea ../features/variants.fea extendable.fea ../features/extensions.fea > Fairfax_base.fea
-cat ../features/languages.fea ../features/sequences.fea joiners.fea asuki.fea                 ../features/slc-variants.fea ../features/variants.fea extendable.fea ../features/extensions.fea > FairfaxPona_base.fea
-cat ../features/languages.fea ../features/sequences.fea joiners.fea atuki.fea                 ../features/slc-variants.fea ../features/variants.fea extendable.fea ../features/extensions.fea > FairfaxPula_base.fea
-cat ../features/languages.fea ../features/sequences.fea joiners.fea ../features/ligatures.fea ../features/slc-variants.fea ../features/variants.fea extendable.fea ../features/extensions.fea > FairfaxHax_base.fea
-rm asuki.fea atuki.fea extendable.fea joiners.fea
-
-$FONTTOOLS feaLib -o Fairfax.ttf Fairfax_base.fea Fairfax_base.ttf
-$FONTTOOLS feaLib -o FairfaxBold.ttf Fairfax_base.fea FairfaxBold_base.ttf
-$FONTTOOLS feaLib -o FairfaxItalic.ttf Fairfax_base.fea FairfaxItalic_base.ttf
-$FONTTOOLS feaLib -o FairfaxSerif.ttf Fairfax_base.fea FairfaxSerif_base.ttf
-$FONTTOOLS feaLib -o FairfaxPona.ttf FairfaxPona_base.fea FairfaxPona_base.ttf
-$FONTTOOLS feaLib -o FairfaxPula.ttf FairfaxPula_base.fea FairfaxPula_base.ttf
-$FONTTOOLS feaLib -o FairfaxHax.ttf FairfaxHax_base.fea FairfaxHax_base.ttf
-$FONTTOOLS feaLib -o FairfaxHaxBold.ttf FairfaxHax_base.fea FairfaxHaxBold_base.ttf
-$FONTTOOLS feaLib -o FairfaxHaxItalic.ttf FairfaxHax_base.fea FairfaxHaxItalic_base.ttf
-$FONTTOOLS feaLib -o FairfaxSerifHax.ttf FairfaxHax_base.fea FairfaxSerifHax_base.ttf
+# Add OpenType features
+$FONTTOOLS feaLib -o Fairfax.ttf ../features/base.fea Fairfax_base.ttf
+$FONTTOOLS feaLib -o FairfaxBold.ttf ../features/base.fea FairfaxBold_base.ttf
+$FONTTOOLS feaLib -o FairfaxItalic.ttf ../features/base.fea FairfaxItalic_base.ttf
+$FONTTOOLS feaLib -o FairfaxSerif.ttf ../features/base.fea FairfaxSerif_base.ttf
+$FONTTOOLS feaLib -o FairfaxPona.ttf ../features/asuki.fea FairfaxPona_base.ttf
+$FONTTOOLS feaLib -o FairfaxPula.ttf ../features/atuki.fea FairfaxPula_base.ttf
+$FONTTOOLS feaLib -o FairfaxHax.ttf ../features/code.fea FairfaxHax_base.ttf
+$FONTTOOLS feaLib -o FairfaxHaxBold.ttf ../features/code.fea FairfaxHaxBold_base.ttf
+$FONTTOOLS feaLib -o FairfaxHaxItalic.ttf ../features/code.fea FairfaxHaxItalic_base.ttf
+$FONTTOOLS feaLib -o FairfaxSerifHax.ttf ../features/code.fea FairfaxSerifHax_base.ttf
 cp FairfaxSM_base.ttf FairfaxSM.ttf
 cp FairfaxSMBold_base.ttf FairfaxSMBold.ttf
 cp FairfaxSMItalic_base.ttf FairfaxSMItalic.ttf
 cp FairfaxSerifSM_base.ttf FairfaxSerifSM.ttf
 
-rm *_base.fea
+# Remove intermediate artifacts
+rm *_base.kbitx
+rm ../features/spascii.fea
+rm ../features/spbase.fea
+rm ../features/tpascii.fea
 rm *_base.ttf
 
 # Inject PUAA table
